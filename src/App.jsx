1d0074eb85c8427a21f19bcff7c5928299d6e7e6
mihxw <mishamishavetrov@gmail.com>
2025-03-04 18:47:26 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Feedback from "./components/Feedback/Feedback.jsx";
 import Options from "./components/Options/Options.jsx";
 import Notification from "./components/Notification/Notification.jsx";
+import Description from "./components/Description.jsx";
 
 function App() {
   const [feedback, setFeedback] = useState(() => {
@@ -22,22 +23,19 @@ function App() {
     }));
   };
 
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-
   const resetFeedback = () => {
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const positiveFeedback =
     totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   return (
     <div>
       <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <Description />
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
 
       <Options
         updateFeedback={updateFeedback}
@@ -59,15 +57,3 @@ function App() {
 }
 
 export default App;
-
-import Description from './components/Description';
-
-function App() {
-    return (
-        <div>
-            <h1>Sip Happens Café</h1>
-            <Description />
-            {/* Інші компоненти */}
-        </div>
-    );
-}
